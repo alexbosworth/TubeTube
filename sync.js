735a4472ea4f4e9a6e91459af630b66d80d41d12
Alex Bosworth <alex.bosworth@gmail.com>
2011-04-07 12:13:50 +0800

    sync now does two phase sync successfully

--- sync.js
+++ sync.js
@@ -1,4 +1,4 @@
-var readFileSync = require('fs').readFileSync,
+var readFile = require('fs').readFile,
     KEYS = require('./config/keys'),
     httpGet = require('http').get,
     parseUrl = require('url').parse,
@@ -19,29 +19,33 @@ function SyncVideo(in_url) {
         in_url
     ];
     
-    var filename = null;
+    var output = '';
     
     var sync = spawnChildProcess(__dirname + '/youtube-dl/youtube-dl', [in_url]);
     
-    sync.stdout.on('data', function downloadingCbk(chunk) { 
-        chunk = chunk+'';
-        
-        if (/\[download\] Destination: /gim.test(chunk)) {
-            filename = chunk.slice(24);
-        }
-        
-        console.log(chunk);
+    sync.stdout.on('data', function downloadingCbk(chunk) { output += chunk; 
+        console.log(chunk+'');
     });
     
     sync.on('exit', function downloadingComplete() {
-        setTimeout(function startSyncing() { 
-            var data = readFileSync(__dirname + '/' + filename);
+        var filename = null;
         
+        output.split('\n').forEach(function checkLine(line) {
+            if (/\[download\] Destination: /gim.test(line) == false) return;
+            
+            filename = line.slice(24);            
+        });
+                
+        if (!filename) return console.log("NO VIDEO FOUND");
+                
+        readFile(__dirname + '/' + filename, 
+        
+        function fileArrived(err, data) {
             storage().put(filename, {binaryBuffer: data}).
-        
+
             on('success', function() { 
-                console.log(filename, 'stored on S3');
-            });
-        }, 2000);            
+                console.log(filename, 'STORED ON S3');
+            });            
+        });
     });
 }
