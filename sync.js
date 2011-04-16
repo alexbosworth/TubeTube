81036ec8041190a19d897e4b6a6dc020e25a5742
Alex <alex.bosworth@gmail.com>
2011-04-17 01:41:56 +0800

    miscommit

--- sync.js
+++ sync.js
@@ -3,8 +3,8 @@ var KEYS = require('./config/keys'),
     SyncVideo = require('./lib/syncVideo').SyncVideo,
     FindVideos = require('./lib/findVideos').FindVideos;
   
-new FindVideos(process.argv[2], __dirname, function foundVideos(vids) {
+new FindVideos(process.argv[2], function foundVideos(vids) {
     vids.forEach(function(vid) {
-        new SyncVideo(storage, vid)
+        new SyncVideo(storage, __dirname, vid)
     });
 })
